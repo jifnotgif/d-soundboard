8ddc8e69e6470591fdee9efc19117ccc954b22e6
jifnotgif <david.doo@stonybrook.edu>
2018-06-07 23:28:21 -0400

    added pre-gain, sound analysis broken

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
 
 // window.addEventListener('load', init, false);
 
-var audio;
+var audio = new Audio();
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioCtx = new AudioContext();
 var audioSource = document.getElementsByClassName("audio-in")[0];
 var panInput = document.querySelector(".pan");
 var muteInput = document.querySelector(".mute");
 var channelVolumeInput = document.querySelector(".channel-volume");
+var initGainInput = document.querySelector(".gain");
 // var audio = document.querySelector('audio');
 var panNode;
-var gainNode = audioCtx.createGain();
+var preAmp = audioCtx.createGain();
+var channelFader = audioCtx.createGain();
+var clipAnalyser = audioCtx.createAnalyser();
 var src;
+var testArray = new Float32Array(clipAnalyser.frequencyBinCount);
 audioSource.addEventListener("change", function(){
 
 // var volume = audioCtx.createGain();
 if(audioSource.options[audioSource.selectedIndex].value === "assets/sounds/drum.mp3"){
-		audio = new Audio(document.querySelector('audio').src);
+		audio.src = "assets/sounds/drum.mp3";
+
 		src = audioCtx.createMediaElementSource(audio);
- 		
   	// 	gainNode.gain.value =0.1;
-		src.connect(gainNode)
- 	 	gainNode.connect(audioCtx.destination);	
+
+  		src.connect(preAmp);
+  		preAmp.connect(clipAnalyser);
+  		clipAnalyser.connect(audioCtx.destination);
+
+
+
+		// src.connect(channelFader)
+
+
+
+ 	//  	channelFader.connect(audioCtx.destination);	
 		audio.play();
 	}
 });
@@ -32,25 +46,35 @@ panInput.addEventListener("input", function(){
 	panNode = audioCtx.createStereoPanner();
 	panNode.pan.setValueAtTime( panInput.value ,audioCtx.currentTime);
 	src.connect(panNode);
-	panNode.connect(gainNode);
-	gainNode.connect(audioCtx.destination);	
+	panNode.connect(channelFader);
+	channelFader.connect(audioCtx.destination);	
 	// audio.play();
 });
 
 muteInput.addEventListener("click", function(){
 	if(muteInput.id ==""){
-		gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+		channelFader.gain.setValueAtTime(0, audioCtx.currentTime);
     	muteInput.id = "active";
 	}
 	else{
-		gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
+		channelFader.gain.setValueAtTime(1, audioCtx.currentTime);
     	muteInput.id = "";
 	}
 });
 
 channelVolumeInput.addEventListener("input", function(){
-	gainNode.gain.setValueAtTime( dBFSToGain(channelVolumeInput.value) , audioCtx.currentTime);
+	channelFader.gain.setValueAtTime( dBFSToGain(channelVolumeInput.value) , audioCtx.currentTime);
 });
+
+initGainInput.addEventListener("input", function(){
+	preAmp.gain.setValueAtTime( dBFSToGain(initGainInput.value) , audioCtx.currentTime);
+});
+		
+
+		audio.addEventListener("playing", function(){
+ 			clipAnalyser.getFloatFrequencyData(testArray);
+ 			console.log(testArray);
+ 		});
 
 function dBFSToGain(dbfs) {
   return Math.pow(10, dbfs / 20);
